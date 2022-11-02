d360763daac954dcbb10c48b4bf641404e1d84f4
PabloMera6 <pabmergom@alum.us.es>
2022-11-02 18:53:48 +0100

    fin

--- src/main/java/org/springframework/samples/petclinic/product/ProductTypeFormatter.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductTypeFormatter.java
@@ -1,24 +1,37 @@
 package org.springframework.samples.petclinic.product;
 
 import java.text.ParseException;
+import java.util.Collection;
 import java.util.Locale;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.Formatter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProductTypeFormatter implements Formatter<ProductType>{
 
+    private final ProductService prService;
+
+	@Autowired
+	public ProductTypeFormatter(ProductService productService) {
+		this.prService = productService;
+	}
+
     @Override
     public String print(ProductType object, Locale locale) {
-        // TODO Auto-generated method stub
-        return null;
+        return object.getName();
     }
 
     @Override
     public ProductType parse(String text, Locale locale) throws ParseException {
-        // TODO Auto-generated method stub
-        return null;
+        Collection<ProductType> findProductTypes = this.prService.findAllProductTypes();
+		for (ProductType type : findProductTypes) {
+			if (type.getName().equals(text)) {
+				return type;
+			}
+		}
+		throw new ParseException("type not found: " + text, 0);
     }
     
 }
